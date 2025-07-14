789b1338980f43c912e9b7babe518bea716bf503
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-14 13:32:03 -0400

    Create the playGame function
    The playGame functions calls the playRound
    function 5 times to compare player and computer
    choices and update scores exactly 5 times.

--- app.js
+++ app.js
@@ -28,22 +28,22 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === "rock") {
     if (computerChoice === "scissors") {
-      console.log(`You chose: ${human}`);
-      console.log(`Computer chose: ${computer}`);
+      console.log(`You chose: ${humanSelection}`);
+      console.log(`Computer chose: ${computerSelection}`);
       console.log("You win! Rock beats scissors.");
       playerScore++;
       console.log(`Your score: ${playerScore}`);
       console.log(`Computer score: ${computerScore}`);
     } else if (computerChoice === "paper") {
-      console.log(`You chose: ${human}`);
-      console.log(`Computer chose: ${computer}`);
+      console.log(`You chose: ${humanSelection}`);
+      console.log(`Computer chose: ${computerSelection}`);
       console.log("You lose! Paper beats rock.");
       computerScore++;
       console.log(`Your score: ${playerScore}`);
       console.log(`Computer score: ${computerScore}`);
     } else {
-      console.log(`You chose: ${human}`);
-      console.log(`Computer chose: ${computer}`);
+      console.log(`You chose: ${humanSelection}`);
+      console.log(`Computer chose: ${computerSelection}`);
       console.log("It's a tie");
       console.log(`Your score: ${playerScore}`);
       console.log(`Computer score: ${computerScore}`);
@@ -51,22 +51,22 @@ function playRound(humanChoice, computerChoice) {
   } 
   if (humanChoice === "paper") {
     if (computerChoice === "rock") {
-      console.log(`You chose: ${human}`);
-      console.log(`Computer chose: ${computer}`);
+      console.log(`You chose: ${humanSelection}`);
+      console.log(`Computer chose: ${computerSelection}`);
       console.log("You win! Paper beats rock.");
       playerScore++;
       console.log(`Your score: ${playerScore}`);
       console.log(`Computer score: ${computerScore}`);
     } else if (computerChoice === "scissors") {
-      console.log(`You chose: ${human}`);
-      console.log(`Computer chose: ${computer}`);
+      console.log(`You chose: ${humanSelection}`);
+      console.log(`Computer chose: ${computerSelection}`);
       console.log("You lose! Scissors beats paper.");
       computerScore++;
       console.log(`Your score: ${playerScore}`);
       console.log(`Computer score: ${computerScore}`);
     } else {
-      console.log(`You chose: ${human}`);
-      console.log(`Computer chose: ${computer}`);
+      console.log(`You chose: ${humanSelection}`);
+      console.log(`Computer chose: ${computerSelection}`);
       console.log("It's a tie");
       console.log(`Your score: ${playerScore}`);
       console.log(`Computer score: ${computerScore}`);
@@ -75,22 +75,22 @@ function playRound(humanChoice, computerChoice) {
 
   if (humanChoice === "scissors") {
     if (computerChoice === "paper") {
-      console.log(`You chose: ${human}`);
-      console.log(`Computer chose: ${computer}`);
+      console.log(`You chose: ${humanSelection}`);
+      console.log(`Computer chose: ${computerSelection}`);
       console.log("You win! Scissors beats paper.");
       playerScore++;
       console.log(`Your score: ${playerScore}`);
       console.log(`Computer score: ${computerScore}`);
     } else if (computerChoice === "rock") {
-      console.log(`You chose: ${human}`);
-      console.log(`Computer chose: ${computer}`);
+      console.log(`You chose: ${humanSelection}`);
+      console.log(`Computer chose: ${computerSelection}`);
       console.log("You lose! Rock beats scissors.");
       computerScore++;
       console.log(`Your score: ${playerScore}`);
       console.log(`Computer score: ${computerScore}`);
     } else {
-      console.log(`You chose: ${human}`);
-      console.log(`Computer chose: ${computer}`);
+      console.log(`You chose: ${humanSelection}`);
+      console.log(`Computer chose: ${computerSelection}`);
       console.log("It's a tie");
       console.log(`Your score: ${playerScore}`);
       console.log(`Computer score: ${computerScore}`);
@@ -98,11 +98,27 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-let human = getHumanChoice(); 
+function playGame(human, computer) {
+  playRound(human, computer);
+  human = getHumanChoice();
+  computer = getComputerChoice();
+  playRound(human, computer);
+  human = getHumanChoice();
+  computer = getComputerChoice();
+  playRound(human, computer);
+  human = getHumanChoice();
+  computer = getComputerChoice();
+  playRound(human, computer);
+  human = getHumanChoice();
+  computer = getComputerChoice();
+  playRound(human, computer);
+}
 
-let computer = getComputerChoice();
+let humanSelection = getHumanChoice(); 
+
+let computerSelection = getComputerChoice();
 
 console.log(`Original player score: ${playerScore}`);
 console.log(`Original computer score: ${computerScore}`);
 
-playRound(human, computer);
+playGame(humanSelection, computerSelection);
