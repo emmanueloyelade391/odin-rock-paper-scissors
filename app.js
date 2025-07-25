6755ac051e1491d005090ed0bb05115a76ed24dd
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-24 20:29:10 -0400

    Delete code thats no longer used or neccessary.
    I deleted the playGame function declaration and
    tolowerCase function that was applied to the
    human choice because these portions of code were
    made redundant with the user interaction being
    on the screen with buttons instead of a prompt in
    the console.

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    //humanChoice = humanChoice.toLowerCase();
     if (humanChoice === "rock") {
       if (computerChoice === "scissors") {
         console.log(`You chose: ${humanChoice}`);
@@ -94,105 +93,6 @@ function playRound(humanChoice, computerChoice) {
         }
       }
     }
-
-//playGame() runs playRound() 5 times and announces the definitive 
-//winner of the game after playRound() is ran 5 times.
-/*function playGame() {
-  // playRound() compares the player's choice to the computer's choice and
-  // decides who wins. Then the scores are updated and everything is printed
-  // to the browser console.
-  function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice === "rock") {
-      if (computerChoice === "scissors") {
-        console.log(`You chose: ${humanChoice}`);
-        console.log(`Computer chose: ${computerChoice}`);
-        console.log("You win! Rock beats scissors.");
-        playerScore++;
-        console.log(`Your score: ${playerScore}`);
-        console.log(`Computer score: ${computerScore}`);
-      } else if (computerChoice === "paper") {
-        console.log(`You chose: ${humanChoice}`);
-        console.log(`Computer chose: ${computerChoice}`);
-        console.log("You lose! Paper beats rock.");
-        computerScore++;
-        console.log(`Your score: ${playerScore}`);
-        console.log(`Computer score: ${computerScore}`);
-      } else {
-        console.log(`You chose: ${humanChoice}`);
-        console.log(`Computer chose: ${computerChoice}`);
-        console.log("It's a tie");
-        console.log(`Your score: ${playerScore}`);
-        console.log(`Computer score: ${computerScore}`);
-      }
-      } 
-      if (humanChoice === "paper") {
-        if (computerChoice === "rock") {
-          console.log(`You chose: ${humanChoice}`);
-          console.log(`Computer chose: ${computerChoice}`);
-          console.log("You win! Paper beats rock.");
-          playerScore++;
-          console.log(`Your score: ${playerScore}`);
-          console.log(`Computer score: ${computerScore}`);
-        } else if (computerChoice === "scissors") {
-          console.log(`You chose: ${humanChoice}`);
-          console.log(`Computer chose: ${computerChoice}`);
-          console.log("You lose! Scissors beats paper.");
-          computerScore++;
-          console.log(`Your score: ${playerScore}`);
-          console.log(`Computer score: ${computerScore}`);
-        } else {
-          console.log(`You chose: ${humanChoice}`);
-          console.log(`Computer chose: ${computerChoice}`);
-          console.log("It's a tie");
-          console.log(`Your score: ${playerScore}`);
-          console.log(`Computer score: ${computerScore}`);
-        }
-      }
-
-      if (humanChoice === "scissors") {
-        if (computerChoice === "paper") {
-          console.log(`You chose: ${humanChoice}`);
-          console.log(`Computer chose: ${computerChoice}`);
-          console.log("You win! Scissors beats paper.");
-          playerScore++;
-          console.log(`Your score: ${playerScore}`);
-          console.log(`Computer score: ${computerScore}`);
-        } else if (computerChoice === "rock") {
-          console.log(`You chose: ${humanChoice}`);
-          console.log(`Computer chose: ${computerChoice}`);
-          console.log("You lose! Rock beats scissors.");
-          computerScore++;
-          console.log(`Your score: ${playerScore}`);
-          console.log(`Computer score: ${computerScore}`);
-        } else {
-          console.log(`You chose: ${humanChoice}`);
-          console.log(`Computer chose: ${computerChoice}`);
-          console.log("It's a tie");
-          console.log(`Your score: ${playerScore}`);
-          console.log(`Computer score: ${computerScore}`);
-        }
-      }
-    }
-
-  //Create player and computer score and set them to 0
-  let playerScore = 0;
-  let computerScore = 0;
-  console.log(`Original player score: ${playerScore}`);
-  console.log(`Original computer score: ${computerScore}`);
-
-  playRound(getHumanChoice(), getComputerChoice());
-
-  if (playerScore > computerScore) {
-    console.log("You win!");
-  } else if (playerScore === computerScore) {
-    console.log("It's an overall tie!");
-  } else {
-    console.log("You lose! The computer wins!");
-  }
-} */
-
-//playGame();
 
 let playerScore = 0;
 let computerScore = 0;
